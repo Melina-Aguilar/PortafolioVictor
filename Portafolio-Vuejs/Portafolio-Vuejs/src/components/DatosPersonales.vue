866eb0f7145455c2abbd99a08d623aa17750bfd1
Melina Aguilar <aguilarmelinaelizabeth@gmail.com>
2024-11-11 00:27:11 -0300

    Agregue carpeta

---
+++ Portafolio-Vuejs/Portafolio-Vuejs/src/components/DatosPersonales.vue
@@ -0,0 +1,92 @@
+<script setup>
+    import linkedin from "/src/assets/linkedin_icon.svg";
+    import instagram from "/src/assets/instagram_icon.svg";
+    import github from "/src/assets/github_icon.svg";
+    import cv from "/src/assets/cv_resume_icon.svg";
+
+    const title = "Victor Alejandro Aguilar";
+    const descripcion = "Tecnico Univercitario en programación - UTN";
+    const residencia = "San Miguel, Tucuman, Argentica";
+    const presentacion = "Hola, Bienvenido a mi portafolio de proyectos. Soy un desarrollador web con experiencia en el desarrollo de aplicaciones web y moviles"
+    const redesSociales = [
+    {id: 1, name: "linkedin", src: linkedin, url: "htpps://www.linkendin.com/in/jose-perez/"},
+    {id: 2, name: "Intagran", src: instagram, url: "https://www.instragam.com/joseperez"},
+    {id: 3, name: "github", src: github, url: "https://github.com/a-le05"},
+    {id: 4, name: "curriculum", src : cv, url: ""},
+    ];
+    const telefono = "victoralejandroaguilar46@gmail.com"
+</script>
+
+<template>
+    <section class="datos-personales">
+        <div class="card">
+            <h1>{{ title }}</h1>
+            <h2>{{ descripcion }}</h2>
+            <p>{{ presentacion }}</p>
+            <ul class="container-lista">
+                <li v-for="red in redesSociales" :key="red.id">
+                    <a :href="red.url"><img class="icon-redsocial" :src="red.src" width="35rem" :alt="red.name"></a>
+                </li>
+            </ul>
+            <h3> Contacto: {{ telefono }}</h3>
+            <h4>{{ residencia }}</h4>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Doto:wght@400;700&display=swap');
+h1 {
+    font-size: 2.5rem;
+}
+
+p {
+    font-size: 1.2rem;
+    font-weight: 600;
+    margin-bottom: 1rem;
+}
+
+.card {
+    
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); 
+    color: #e9e7e7;
+    font-family: 'Doto', sans-serif;
+    background-color: rgb(50, 50, 50);
+    border-radius: 10px;
+    padding: 10px;
+    text-align: center;
+    font-weight: 800; 
+    
+}
+
+.container-lista {
+    display: flex;
+    justify-content: center;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    text-align: center;
+}
+
+.icon-redsocial {
+    align-items: center;
+    background-color: aliceblue;
+    border-radius: 50%;
+    padding: 2px;
+    margin: 5px;
+    box-shadow: 0 0 5px rgba(95, 124, 205, 0.934);
+}
+
+.icon-redsocial:hover {
+    
+    background-color: rgb(0, 0, 0);
+    box-shadow: 0 0 5px rgba(251, 249, 249, 0.934);
+}
+
+h3 {
+    
+    font-size: 1.2rem;
+    font-weight: 600;
+}
+
+</style>
---
+++ Portafolio-Vuejs/Portafolio-Vuejs/src/components/NavBar.vue
@@ -0,0 +1,70 @@
+<template>
+    <nav class="navbar">
+      <div class="navbar-menu">
+        <ul>
+            <a v-for="nav in navegacion" :key="nav.nombre" :href="nav.enlace" class="nav-item" >{{nav.nombre}}</a> 
+        </ul>
+      </div>
+    </nav>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+/*Tenemos creado un arreglo de objetos*/
+const navegacion= ref([
+ {id:1, nombre:'Educación', enlace:'#educacion'},
+ {id:2, nombre:'Experiencia', enlace:'#experiencia'},
+ {id:3, nombre:'Proyectos', enlace:'#proyectos'},
+ {id:4, nombre:'Habilidades', enlace:'#habilidades'},
+ {id:5, nombre:'Intereses', enlace:'#intereses'}
+]);
+</script>
+
+<style scoped>
+.navbar {
+  background-color: var(--vt-c-indigo);
+  color: #000000;
+  padding: 0.5rem 1 rem;
+  align-items: center;
+}
+.navbar-item {
+  color: #0c0c0c;
+  text-decoration: none;
+  margin-right: 1rem;
+}
+.navbar-menu {
+  display: flex;
+  justify-content: flex-end;
+}
+.nav-list {
+  list-style: none;
+}
+a {
+  font-size: 1.2rem;
+  font-weight: 600;
+  color: #c8cc8d;
+  border: 1px solid;
+  border-color: hsla(0, 0%, 0%, 0.534);
+  border-radius: 16px;
+  /*Eliminar el subrayado de los enlaces y elmentos con clase "green" */
+  font-family: 'Doto', sans-serif;
+  /* Añade una transición suave para los cambios de estilo */
+  
+  /* Añade padding alrededor del contenido */
+  padding: 9px;
+}
+
+a:hover {
+  /* Cambia el color de fondo al pasar el mause sobre un enlace */
+  background-color: rgb(28, 41, 52);
+}
+
+@media (max-width: 768px) {
+  .navbar-menu {
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+  }
+}
+
+</style>
